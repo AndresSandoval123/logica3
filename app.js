8aff3b276c2388328b1b8fb557fc3c9cac13ae02
Andres.Sandoval123 <andres.sandoval@genstudents.org>
2024-06-20 00:00:37 -0500

    funcionalidad ok

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
+/* Funcion factorizar, se encargara de devolver el factorial, del numero ingresado */
 
 function factorizar(num){
+
+    /* La funcion entra a directamente a un IF, es el que se encargara de hacer la validación  */
     if(num == 0 || num == 1){
         return(1);
     }
     else if(num > 1){
         for(let i = num - 1; i > 1; i--){
-            resul = i * num
+            num = num * i
         }
-        
+        return num
     }else if(!num){
         alert("No es un numero");
     }
